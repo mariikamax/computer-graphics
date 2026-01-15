022a74e2c577bda22e1ecabf73fd2bd5a39801ae
Maria Maximova <maximova.m06@mail.ru>
2026-01-15 21:56:48 +0300

    длбавлена автоматическая триаунгуляция и пересчет нормалей

--- src/main/java/com/cgvsu/GuiController.java
+++ src/main/java/com/cgvsu/GuiController.java
@@ -1,5 +1,6 @@
 package com.cgvsu;
 
+import com.cgvsu.model.ModelUtils;
 import com.cgvsu.render_engine.RenderEngine;
 import javafx.fxml.FXML;
 import javafx.animation.Animation;
@@ -87,9 +88,11 @@ public class GuiController {
         try {
             String fileContent = Files.readString(fileName);
             mesh = ObjReader.read(fileContent);
-            // todo: обработка ошибок
-        } catch (IOException exception) {
 
+            ModelUtils.triangulate(mesh);
+            ModelUtils.calculateNormals(mesh);
+
+        } catch (IOException exception) {
         }
     }
 
