f3c125022268b9903fc60c77a5530a73daff1dbf
Dasha <makashovadasha21@mail.ru>
2026-01-16 20:28:35 +0300

    немного изменены   классы модели и математики

--- src/main/java/com/cgvsu/model/Model.java
+++ src/main/java/com/cgvsu/model/Model.java
@@ -1,13 +1,30 @@
 package com.cgvsu.model;
-import com.cgvsu.math.Vector2f;
-import com.cgvsu.math.Vector3f;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class Model {
+    public ArrayList<com.cgvsu.math.Vector3f> vertices = new ArrayList<>();
+    public ArrayList<com.cgvsu.math.Vector2f> textureVertices = new ArrayList<>();
+    public ArrayList<com.cgvsu.math.Vector3f> normals = new ArrayList<>();
+    public ArrayList<Polygon> polygons = new ArrayList<>();
 
-    public ArrayList<Vector3f> vertices = new ArrayList<Vector3f>();
-    public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
-    public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
-    public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+    private String fileName;
+    private boolean visible = true;
+
+    // Геттеры и сеттеры для новых полей
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
 }
